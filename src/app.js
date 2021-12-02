37a1fccf48af4f9ce3b094c0ecd460e1bb533919
DavidALevy <david.levy.levy@btinternet.com>
2021-12-02 14:11:19 +0000

    added crud fuctions

--- src/app.js
+++ src/app.js
@@ -3,7 +3,12 @@ const {
     addMovie,
     listMovies,
     deleteByTitle,
-    updateRating
+    updateRating,
+    updateActor,
+    updateYear,
+    updateTitle,
+    findByTitle,
+    findByActor
 } = require("./utils");
 
 const command = process.argv[2];
@@ -22,8 +27,21 @@ const app = async () => {
         await connection(deleteByTitle,{title:process.argv[3]})
     } else if(command==="updateRating"){
         await connection(updateRating,{title:process.argv[3]},{rating:process.argv[4]})
+    } else if(command==="updateActor"){
+        await connection(updateActor,{title:process.argv[3]},{actor:process.argv[4]})
     } 
-       
+    else if(command==="updateYear"){
+        await connection(updateYear,{title:process.argv[3]},{year:process.argv[4]})
+    } 
+    else if(command==="updateTitle"){
+        await connection(updateTitle,{title:process.argv[3]},{title:process.argv[4]})
+    }   
+    else if(command==="findByTitle"){
+        await connection(findByTitle,{title:process.argv[3]})
+    } 
+    else if(command==="findByActor"){
+        await connection(findByActor,{actor:process.argv[3]})
+    } 
     else {
         console.log("Incorrect Input")
     }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,15 @@
 exports.addMovie = async (collection, dataObj) => {
     try{
         await collection.insertOne(dataObj)
+        console.log(`The film <${dataObj.title}> has been added to your collection.`)
     } catch (error){
         console.log(error)
     }
 };
 
 exports.listMovies = async (collection) => {
-    try {       
+    try {  
+        console.log("Here is the conplete list of films in this collection:")     
         console.log(await collection.find().toArray())
     } catch (error){
         console.log(error);
@@ -16,7 +18,7 @@ exports.listMovies = async (collection) => {
 exports.deleteByTitle = async (collection,dataObj) => {
     try {       
         await collection.deleteMany( dataObj )
-        console.log(dataObj)
+        console.log(`All instances of the fimm entitled <${dataObj}> has been deleted.`)
     } catch (error){
         console.log(error);
     }
@@ -24,10 +26,49 @@ exports.deleteByTitle = async (collection,dataObj) => {
 exports.updateRating = async (collection,dataObj,updateObj) => {
     try {       
         await collection.updateMany( dataObj,{$set: updateObj })
-        console.log(dataObj)
-        console.log(updateObj)
-        
+        console.log(`The film entitled <${dataObj.title}> has updated <actor> to <${updateObj.rating}>`)   
     } catch (error){
         console.log(error);
     }
 };
+exports.updateActor = async (collection,dataObj,updateObj) => {
+    try {       
+        await collection.updateMany( dataObj,{$set: updateObj })
+        console.log(`The film entitled <${dataObj.title}> has updated <actor> to <${updateObj.actor}>`)
+            } catch (error){
+        console.log(error);
+    }
+};
+exports.updateYear = async (collection,dataObj,updateObj) => {
+    try {       
+        await collection.updateMany( dataObj,{$set: updateObj })
+        console.log(`The film entitled <${dataObj.title}> has updated <year> to <${updateObj.year}>`)
+            } catch (error){
+        console.log(error);
+    }
+};
+exports.updateTitle = async (collection,dataObj,updateObj) => {
+    try {       
+        await collection.updateMany( dataObj,{$set: updateObj })
+        console.log(`The film entitled <${dataObj.title}> has updated <title> to <${updateObj.title}>`)
+            } catch (error){
+        console.log(error);
+    }
+};
+exports.findByTitle = async (collection,dataObj) => {
+    try {       
+        console.log(await collection.find(dataObj).toArray())        
+            } catch (error){
+        console.log(error);
+    }
+};
+exports.findByActor = async (collection,dataObj) => {
+    try {       
+        console.log(await collection.find(dataObj).toArray())        
+         console.log(dataObj) 
+          }             
+            catch (error){
+        console.log(error);
+    }
+};
+
